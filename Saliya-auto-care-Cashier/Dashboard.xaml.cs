054c68ee7b485d5c4ae1af05974ea7f26dbc1f68
Sachitha Samadhi Bandara <sachithasbandara6@gmail.com>
2024-10-04 13:25:48 +0530

    add stuff

--- Saliya-auto-care-Cashier/Dashboard.xaml.cs
+++ Saliya-auto-care-Cashier/Dashboard.xaml.cs
@@ -1,22 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Saliya_auto_care_Cashier
 {
-    /// <summary>
-    /// Interaction logic for Dashboard.xaml
-    /// </summary>
     public partial class Dashboard : Window
     {
         public Dashboard()
@@ -26,7 +12,14 @@ namespace Saliya_auto_care_Cashier
 
         private void btn_home(object sender, RoutedEventArgs e)
         {
-            fContainer.Navigate(new System.Uri("MVVM/View/registorview.xaml", UriKind.RelativeOrAbsolute));
+            try
+            {
+                fContainer.Navigate(new System.Uri("MVVM/View/registorview.xaml", UriKind.RelativeOrAbsolute));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error navigating to page: {ex.Message}");
+            }
         }
     }
 }
