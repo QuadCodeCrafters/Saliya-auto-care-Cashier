d968fed3602323861095b4b63a76e95e53af1492
Sachitha Samadhi Bandara <sachithasbandara6@gmail.com>
2024-10-04 16:35:22 +0530

    Update Loginpage.xaml.cs

--- Saliya-auto-care-Cashier/Loginpage.xaml.cs
+++ Saliya-auto-care-Cashier/Loginpage.xaml.cs
@@ -1,1 +1,36 @@
 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Saliya_auto_care_Cashier
+{
+    /// <summary>
+    /// Interaction logic for Loginpage.xaml
+    /// </summary>
+    public partial class Loginpage : Window
+    {
+        public Loginpage()
+        {
+           InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Welcomepage w = new Welcomepage();
+            w.Show();
+            this.Close();
+        }
+    }
+}
+
