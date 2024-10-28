09d77a2379901643dc593020620ca272b4e190df
Sachitha Samadhi Bandara <sachithasbandara6@gmail.com>
2024-10-29 02:13:09 +0530

    add stuff

--- Saliya-auto-care-Cashier/MVC/View/Register_View.xaml.cs
+++ Saliya-auto-care-Cashier/MVC/View/Register_View.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 
 namespace Saliya_auto_care_Cashier.MVVM.View
 {
@@ -20,9 +13,104 @@ namespace Saliya_auto_care_Cashier.MVVM.View
     /// </summary>
     public partial class Register_View : UserControl
     {
+        private List<Control> requiredFields;
+
         public Register_View()
         {
             InitializeComponent();
+            InitializeRequiredFields();
+        }
+
+        private void InitializeRequiredFields()
+        {
+            requiredFields = new List<Control>
+            {
+                txtvehiclenum, txtvehicletype, txtvehiclemodel, txtcusname,
+                txtcusaddress, txtcusNIC, txtcusmail, txtcusnumber, txtcusspec
+            };
+        }
+
+        private void btn_registor(object sender, RoutedEventArgs e)
+        {
+            if (IsAnyFieldEmpty())
+            {
+                ShowErrorAnimation();
+            }
+            else
+            {
+                
+            }
+        }
+
+        private bool IsAnyFieldEmpty()
+        {
+            foreach (var field in requiredFields)
+            {
+                if (field is TextBox textBox && string.IsNullOrWhiteSpace(textBox.Text) ||
+                    field is ComboBox comboBox && comboBox.SelectedItem == null)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void ShowErrorAnimation()
+        {
+            foreach (var field in requiredFields)
+            {
+                bool hasError = (field is TextBox textBox && string.IsNullOrWhiteSpace(textBox.Text)) ||
+                                (field is ComboBox comboBox && comboBox.SelectedItem == null);
+
+                if (hasError)
+                {
+                    ApplyErrorAppearance(field);
+                    ShakeControl(field);
+                }
+            }
+
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3)//timer
+            };
+            timer.Tick += (s, e) =>
+            {
+                foreach (var field in requiredFields)
+                {
+                    ResetToDefaultAppearance(field);
+                }
+                timer.Stop();
+            };
+            timer.Start();
+        }
+
+        private void ApplyErrorAppearance(Control control)
+        {
+            control.BorderBrush = Brushes.Red;
+            control.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void ShakeControl(Control control)
+        {
+            TranslateTransform translateTransform = new TranslateTransform();
+            control.RenderTransform = translateTransform;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = 0,
+                To = 10,
+                Duration = TimeSpan.FromMilliseconds(50),
+                AutoReverse = true,
+                RepeatBehavior = new RepeatBehavior(5) // Shake 5 times
+            };
+
+            translateTransform.BeginAnimation(TranslateTransform.XProperty, animation);
+        }
+
+        private void ResetToDefaultAppearance(Control control)
+        {
+            control.BorderBrush = (Brush)new BrushConverter().ConvertFromString("#FFDDDDDD");
+            control.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#6e6e6e"));
         }
     }
 }
