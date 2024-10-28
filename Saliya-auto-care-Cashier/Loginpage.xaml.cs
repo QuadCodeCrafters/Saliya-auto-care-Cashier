524278eb4df81518d80e0435d91bd533a25f2ee4
viraj gunasinghe <virajwgunasinghe@gmail.com>
2024-10-29 01:48:55 +0530

    add stuff

--- Saliya-auto-care-Cashier/Loginpage.xaml.cs
+++ Saliya-auto-care-Cashier/Loginpage.xaml.cs
@@ -6,21 +6,74 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using Saliya_auto_care_Cashier.MVVM.Model;
 using System.Security.Cryptography.X509Certificates;
+using Saliya_auto_care_Cashier.MVC.Model;
+using Saliya_auto_care_Cashier.MVC.Controller;
+using System.Data;
+using System.Collections.Generic;
 
 namespace Saliya_auto_care_Cashier
 {
     public partial class Loginpage : Window
     {
+        private LoginPageController controller;
+        private string selectedUsername;
+
         public Loginpage()
         {
             InitializeComponent();
-            
+            controller = new LoginPageController(this);
+            LoadUsernames();
         }
+
+
+        private void LoadUsernames()
+        {
+            List<string> usernames = controller.GetUsernames();
+            UserDisplay.ItemsSource = usernames; // Ensure this is a List<string>
+        }
+
+
+        private void UsernamesDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (UserDisplay.SelectedItem != null)
+            {
+                selectedUsername = UserDisplay.SelectedItem.ToString();
+                // You can perform actions with the selected username here.
+                controller.SelectedUsername = UserDisplay.SelectedItem.ToString();
+            }
+        }
+
+        
+
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        public void ShowSuccessMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public void ShowErrorMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        //public void DisplayData(DataTable dataTable)
+        //{
+        //    UserDisplay.ItemsSource = dataTable.DefaultView;
+        //}
+
 
         private const int MaxPasswordLength = 5; // Set your maximum length
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
+            string password = txtPasswordInput.Password;
+            controller.Login(password); // Pass the password to the controller's Login method
+
             DoubleAnimation fadeOutAnimation = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(0.5));
             fadeOutAnimation.Completed += FadeOutAnimation_Completed;  
             this.BeginAnimation(OpacityProperty, fadeOutAnimation);
@@ -142,5 +195,14 @@ namespace Saliya_auto_care_Cashier
         {
             txtPasswordInput.Password = "";
         }
+
+        private void btn12_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseConnection dbcon = new DatabaseConnection();
+
+            dbcon.Connect();
+        }
+
+        
     }
 }
