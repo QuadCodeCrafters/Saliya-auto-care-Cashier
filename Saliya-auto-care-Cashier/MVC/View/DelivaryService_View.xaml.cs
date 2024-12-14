49cf18133a074a1c4bc805484c8fb2df24ec83cd
Sachitha Samadhi Bandara <sachithasbandara6@gmail.com>
2024-12-14 22:45:24 +0530

    add stuff

--- Saliya-auto-care-Cashier/MVC/View/DelivaryService_View.xaml.cs
+++ Saliya-auto-care-Cashier/MVC/View/DelivaryService_View.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -58,6 +59,18 @@ namespace Saliya_auto_care_Cashier.MVVM.View
             selectedButton.Background = Brushes.White;
         }
 
+        private void btn_pickupclick(object sender, RoutedEventArgs e)
+        {
+            var dashboardWindow = Application.Current.Windows.OfType<Dashboard>().FirstOrDefault();
+            if (dashboardWindow != null)
+            {
+                var dialogHost = dashboardWindow.FindName("MemberDialogHost") as MaterialDesignThemes.Wpf.DialogHost; //the name of the dialog host in the dashboard
+                if (dialogHost != null)
+                {
+                    dialogHost.IsOpen = true;  // Open the dialog
+                }
+            }
+        }
     }
 }
 
